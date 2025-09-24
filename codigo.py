1a7bc64b4df3b3ea30f8ae200bca541d0d547546
renato-sonic-e-games <rennerutame009@gmail.com>
2025-09-24 12:16:12 -0300

    trelorelo trelo lo

--- codigo.py
+++ codigo.py
@@ -1,7 +1,25 @@
 import pyautogui
+import time
 
 # abrir o gugou cromi
 
 pyautogui.press("win")
-pyautogui.write("Google Chrome")
+pyautogui.write("Google")
+pyautogui.press("enter")
+time.sleep(0.5)
+pyautogui.write("http://127.0.0.1:5500/index.html")
+pyautogui.press("enter")
+time.sleep(0.5)
+pyautogui.click(x=496, y=469)
+pyautogui.write("nome nomeado por um nomeador que nomeiou e ainda nomeia e continuará nomeando")
+time.sleep(0.5)
+pyautogui.press("tab")
+pyautogui.write("email@gmail.email")
+time.sleep(0.5)
+pyautogui.press("tab")
+pyautogui.write("senha secreta (código kojima)")
+time.sleep(0.5)
+pyautogui.press("tab")
+pyautogui.press("space")
+pyautogui.press("tab")
 pyautogui.press("enter")
---
+++ posipythonção.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+
+print(pyautogui.size())
+time.sleep(5)
+print(pyautogui.position())
+
